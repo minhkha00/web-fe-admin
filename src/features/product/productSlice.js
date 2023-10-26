8a58f04ac158f6fdcc119a10a6085bd925548de6
LAPTOP-MB6C592R\trieu <meouutv@gmail.com>
2023-10-26 16:35:13 +0700

    first commit

---
+++ src/features/product/productSlice.js
@@ -0,0 +1,129 @@
+import { createSlice, createAsyncThunk, createAction } from "@reduxjs/toolkit";
+import productService from "./productService";
+
+export const getProducts = createAsyncThunk(
+  "product/get-products",
+  async (thunkAPI) => {
+    try {
+      return await productService.getProducts();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const getAProduct = createAsyncThunk(
+  "product/get-product",
+  async (productId,thunkAPI) => {
+    try {
+      return await productService.getProduct(productId);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const createProducts = createAsyncThunk(
+  "product/create-products",
+  async (productData, thunkAPI) => {
+    try {
+      return await productService.createProduct(productData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const updateProduct = createAsyncThunk(
+  "product/update-product",
+  async (id, thunkAPI) => {
+    try {
+      return await productService.s(id);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const resetState = createAction("Reset_all");
+
+const initialState = {
+  products: [],
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+export const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.products = action.payload;
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(getAProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.productName = action.payload.title;
+        state.productDescription = action.payload.description;
+        state.productPrice = action.payload.price;
+        state.productBrand = action.payload.brand;
+        state.productCategory = action.payload.category;
+        state.productTags = action.payload.tags;
+        state.productQuantity = action.payload.quantity;
+        state.productImages = action.payload.images;
+      })
+      .addCase(getAProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(createProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createdProduct = action.payload;
+      })
+      .addCase(createProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(updateProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateProduct.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createdProduct = action.payload;
+      })
+      .addCase(updateProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(resetState, () => initialState);
+  },
+});
+export default productSlice.reducer;
